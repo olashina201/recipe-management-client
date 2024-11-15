b2303718d5dd1ba5fde0822a3bf66613822b6fbb
olashina201 <olashina201@gmail.com>
2024-11-15 21:13:25 +0100

    error pop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -8,7 +11,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <Component {...pageProps} />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
